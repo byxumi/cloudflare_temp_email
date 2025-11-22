3b21a9a39f62197afaf6e4c488edc368cc8445e3
byxumi <wanyang3077@gmail.com>
2025-11-22 22:04:04 +0800

    Update Index.vue

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -2,7 +2,8 @@
 import { defineAsyncComponent, onMounted, watch, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from 'vue-router'
-import { useMessage } from 'naive-ui'
+import { useMessage, NButton, NIcon, NCard, NSpace, NModal } from 'naive-ui'
+import { User } from '@vicons/fa'
 
 import { useGlobalState } from '../store'
 import { api } from '../api'
@@ -19,12 +20,14 @@ import Webhook from './index/Webhook.vue';
 import Attachment from './index/Attachment.vue';
 import About from './common/About.vue';
 import SimpleIndex from './index/SimpleIndex.vue';
+import UserLogin from './user/UserLogin.vue'
 
 const { loading, settings, openSettings, indexTab, globalTabplacement, useSimpleIndex, userJwt } = useGlobalState()
 const message = useMessage()
 const route = useRoute()
 const router = useRouter()
 const isMobile = useIsMobile()
+const showLoginModal = ref(false)
 
 const SendMail = defineAsyncComponent(() => {
   loading.value = true;
@@ -47,6 +50,8 @@ const { t } = useI18n({
       webhookSettings: 'Webhook Settings',
       query: 'Query',
       enterSimpleMode: 'Simple Mode',
+      login: 'User Login',
+      loginTip: 'Please login to access your mailbox'
     },
     zh: {
       mailbox: '收件箱',
@@ -61,6 +66,8 @@ const { t } = useI18n({
       webhookSettings: 'Webhook 设置',
       query: '查询',
       enterSimpleMode: '极简模式',
+      login: '用户登录',
+      loginTip: '请登录以使用邮箱服务'
     }
   }
 });
@@ -114,6 +121,13 @@ const queryMail = () => {
   mailBoxKey.value = Date.now();
 }
 
+// 登录成功后的回调：手动点击登录后才跳转
+const checkLogin = () => {
+  if (userJwt.value) {
+    router.push('/user')
+  }
+}
+
 watch(route, () => {
   if (!route.query.mail_id) {
     showMailIdQuery.value = false;
@@ -122,12 +136,9 @@ watch(route, () => {
   }
 })
 
-onMounted(() => {
-  // [新增] 登录状态检测
-  if (userJwt.value) {
-    router.push('/user')
-    return
-  }
+onMounted(async () => {
+  // [已删除] 自动跳转逻辑
+  // if (userJwt.value) { router.push('/user'); return; }
 
   if (route.query.mail_id) {
     showMailIdQuery.value = true;
@@ -143,57 +154,96 @@ onMounted(() => {
       <SimpleIndex />
     </div>
     <div v-else>
-      <AddressBar />
-      <n-tabs v-if="settings.address" type="card" v-model:value="indexTab" :placement="globalTabplacement">
-        <template #prefix v-if="!isMobile">
-          <n-button @click="useSimpleIndex = true" tertiary size="small">
-            <template #icon>
-              <n-icon>
-                <FullscreenExitOutlined />
-              </n-icon>
-            </template>
-            {{ t('enterSimpleMode') }}
-          </n-button>
-        </template>
-        <n-tab-pane name="mailbox" :tab="t('mailbox')">
-          <div v-if="showMailIdQuery" style="margin-bottom: 10px;">
-            <n-input-group>
-              <n-input v-model:value="mailIdQuery" />
-              <n-button @click="queryMail" type="primary" tertiary>
-                {{ t('query') }}
-              </n-button>
-            </n-input-group>
-          </div>
-          <MailBox :key="mailBoxKey" :showEMailTo="false" :showReply="true" :showSaveS3="openSettings.isS3Enabled"
-            :saveToS3="saveToS3" :enableUserDeleteEmail="openSettings.enableUserDeleteEmail"
-            :fetchMailData="fetchMailData" :deleteMail="deleteMail" />
-        </n-tab-pane>
-        <n-tab-pane name="sendbox" :tab="t('sendbox')">
-          <SendBox :fetchMailData="fetchSenboxData" :enableUserDeleteEmail="openSettings.enableUserDeleteEmail"
-            :deleteMail="deleteSenboxMail" />
-        </n-tab-pane>
-        <n-tab-pane name="sendmail" :tab="t('sendmail')">
-          <SendMail />
-        </n-tab-pane>
-        <n-tab-pane name="accountSettings" :tab="t('accountSettings')">
-          <AccountSettings />
-        </n-tab-pane>
-        <n-tab-pane name="appearance" :tab="t('appearance')">
-          <Appearance :showUseSimpleIndex="true" />
-        </n-tab-pane>
-        <n-tab-pane v-if="openSettings.enableAutoReply" name="auto_reply" :tab="t('auto_reply')">
-          <AutoReply />
-        </n-tab-pane>
-        <n-tab-pane v-if="openSettings.enableWebhook" name="webhook" :tab="t('webhookSettings')">
-          <Webhook />
-        </n-tab-pane>
-        <n-tab-pane v-if="openSettings.isS3Enabled" name="s3_attachment" :tab="t('s3Attachment')">
-          <Attachment />
-        </n-tab-pane>
-        <n-tab-pane v-if="openSettings.enableIndexAbout" name="about" :tab="t('about')">
-          <About />
-        </n-tab-pane>
-      </n-tabs>
+      <div v-if="settings.address">
+        <AddressBar />
+        <n-tabs type="card" v-model:value="indexTab" :placement="globalTabplacement">
+          <template #prefix v-if="!isMobile">
+            <n-button @click="useSimpleIndex = true" tertiary size="small">
+              <template #icon>
+                <n-icon>
+                  <FullscreenExitOutlined />
+                </n-icon>
+              </template>
+              {{ t('enterSimpleMode') }}
+            </n-button>
+          </template>
+          <n-tab-pane name="mailbox" :tab="t('mailbox')">
+            <div v-if="showMailIdQuery" style="margin-bottom: 10px;">
+              <n-input-group>
+                <n-input v-model:value="mailIdQuery" />
+                <n-button @click="queryMail" type="primary" tertiary>
+                  {{ t('query') }}
+                </n-button>
+              </n-input-group>
+            </div>
+            <MailBox :key="mailBoxKey" :showEMailTo="false" :showReply="true" :showSaveS3="openSettings.isS3Enabled"
+              :saveToS3="saveToS3" :enableUserDeleteEmail="openSettings.enableUserDeleteEmail"
+              :fetchMailData="fetchMailData" :deleteMail="deleteMail" />
+          </n-tab-pane>
+          <n-tab-pane name="sendbox" :tab="t('sendbox')">
+            <SendBox :fetchMailData="fetchSenboxData" :enableUserDeleteEmail="openSettings.enableUserDeleteEmail"
+              :deleteMail="deleteSenboxMail" />
+          </n-tab-pane>
+          <n-tab-pane name="sendmail" :tab="t('sendmail')">
+            <SendMail />
+          </n-tab-pane>
+          <n-tab-pane name="accountSettings" :tab="t('accountSettings')">
+            <AccountSettings />
+          </n-tab-pane>
+          <n-tab-pane name="appearance" :tab="t('appearance')">
+            <Appearance :showUseSimpleIndex="true" />
+          </n-tab-pane>
+          <n-tab-pane v-if="openSettings.enableAutoReply" name="auto_reply" :tab="t('auto_reply')">
+            <AutoReply />
+          </n-tab-pane>
+          <n-tab-pane v-if="openSettings.enableWebhook" name="webhook" :tab="t('webhookSettings')">
+            <Webhook />
+          </n-tab-pane>
+          <n-tab-pane v-if="openSettings.isS3Enabled" name="s3_attachment" :tab="t('s3Attachment')">
+            <Attachment />
+          </n-tab-pane>
+          <n-tab-pane v-if="openSettings.enableIndexAbout" name="about" :tab="t('about')">
+            <About />
+          </n-tab-pane>
+        </n-tabs>
+      </div>
+
+      <div v-else class="landing-container">
+        <n-card class="login-card" :bordered="false">
+          <n-space vertical align="center" size="large">
+            <h2 style="margin-bottom: 0; color: #666;">{{ t('loginTip') }}</h2>
+            <n-button type="primary" size="large" round style="width: 200px; height: 50px; font-size: 18px;"
+              @click="showLoginModal = true">
+              <template #icon>
+                <n-icon size="24">
+                  <User />
+                </n-icon>
+              </template>
+              {{ t('login') }}
+            </n-button>
+          </n-space>
+        </n-card>
+      </div>
     </div>
+
+    <n-modal v-model:show="showLoginModal" preset="card" style="width: 400px;" :title="t('login')">
+      <UserLogin @login-success="checkLogin" />
+    </n-modal>
   </div>
 </template>
+
+<style scoped>
+.landing-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 60vh;
+}
+
+.login-card {
+  max-width: 500px;
+  width: 90%;
+  text-align: center;
+  background-color: transparent;
+}
+</style>
